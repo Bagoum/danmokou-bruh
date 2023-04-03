0750a680dfda94761e61d7a51ec427c5adc463c1
Bagoum <reneedebatz@gmail.com>
2023-04-02 20:31:11 -0700

    Update for DMK v10.1.0

--- Plugins/BRuHGameDef.cs
+++ Plugins/BRuHGameDef.cs
@@ -1,5 +1,6 @@
 ﻿using Danmokou;
 using Danmokou.Achievements;
+using Danmokou.Core;
 using Danmokou.Danmaku;
 using Danmokou.GameInstance;
 using UnityEngine;
@@ -8,14 +9,14 @@ namespace BRuH {
 [CreateAssetMenu(menuName = "Data/GameDef/BRuH")]
 public class BRuHGameDef : SceneDanmakuGameDef {
 
-    public override InstanceFeatures MakeFeatures(DifficultySettings d, long? highScore) => new() {
+    public override InstanceFeatures MakeFeatures(DifficultySettings d, InstanceMode m, long? highScore) => new() {
         Score = new ScoreFeatureCreator(highScore),
         Power = new DisabledPowerFeatureCreator(),
         Faith = new FaithFeatureCreator(),
         ItemExt = new LifeItemExtendFeatureCreator(),
         Rank = new DisabledRankFeatureCreator(),
         ScoreExt = new ScoreExtendFeatureCreator(),
-        Meter = new MeterFeatureCreator()
+        Meter = new DisabledMeterFeatureCreator()
     };
 }
 }
